2a03b6cd3b9707b591a71970c17417653f8167b1
Matthew Elliott <melliott@ccistudios.com>
2011-09-06 16:54:34 -0400

    Initial Commit

---
+++ public_html/templates/vaw/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		
+	
+	</head>
+	<body>
+		<div id="header"><div class="container">
+			
+		</div></div>
+		
+		<div id="masthead">
+		</div>
+		
+		<div id="body">
+			<div id="component">
+				
+			</div>
+			<div id="sidebar">
+					
+			</div>	
+		</div>
+		
+		<div id="footer">
+		</div>
+		
+	</body>
+</html>
