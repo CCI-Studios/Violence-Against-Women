886335b5026559abb1a76e34dad0ae4e74ec6e1a
Matthew Elliott <melliott@ccistudios.com>
2011-09-07 16:08:50 -0400

    Done more styling

--- public_html/templates/vaw/index.php
+++ public_html/templates/vaw/index.php
@@ -1,28 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-		
 	
-	</head>
-	<body>
-		<div id="header"><div class="container">
-			
-		</div></div>
+	<link rel="stylesheet" href="templates/vaw/css/template.css" type="text/css" />
+
+</head>
+
+<body class="<?php echo $wide ?>">
+	<div id="header"><div><div class="container">
+		<jdoc:include type="modules" name="header" style="xhtml" />
 		
-		<div id="masthead">
-		</div>
+	<img id="vaw_logo" src="../../images/default/vaw_logo.png" />
+	
+	<div id="navcontainer">
+		<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Member Agencies</a></li>
+		<li><a href="#">Events</a></li>
+		<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
 		
-		<div id="body">
-			<div id="component">
+		
+		
+	</div></div></div>
+		
+	<div id="masthead"><div class="container">
 				
-			</div>
-			<div id="sidebar">
-					
-			</div>	
+		<img id="masthead_img" src="../../images/default/img_main.jpg">		
+		
+		<div id="masthead_ul">
+			<ul>
+				<h1>A Community Working Together To End Violence Against Women</h1>
+				<li>Morbi vel lacus sed magna gravida mattis 
+					eu in nisl.</li>
+				<li>Sed ullamcorper scelerisque ornare.    
+				   Pellentesque convallis aliquam quam eget 
+				   pretium.</li>
+				<li>Praesent eget nisl libero.</li>
+			</ul>
 		</div>
 		
-		<div id="footer">
-		</div>
+	</div></div>
 		
-	</body>
+	<div id="body"><div class="container">
+	<div id="component">
+		<jdoc:include type="modules" name="component" style="xhtml" />
+		
+	</div>
+	<div id="sidebar">
+		<jdoc:include type="modules" name="sidebar" style="xhtml" />
+		
+	</div>
+	<div class="clear"></div>		
+	</div></div>
+		
+	<div id="footer"><div><div class="container">
+	</div></div></div>
+		
+</body>
 </html>
