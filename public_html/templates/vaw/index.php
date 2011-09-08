ca0584f70cc3dea24582df5f029a94352db11452
Matthew Elliott <melliott@ccistudios.com>
2011-09-08 16:58:55 -0400

    Done

--- public_html/templates/vaw/index.php
+++ public_html/templates/vaw/index.php
@@ -1,99 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<jdoc:include type="head" />
 	
 	<link rel="stylesheet" href="templates/vaw/css/template.css" type="text/css" />
-
 </head>
 
 <body class="<?php echo $wide ?>">
 	<div id="header"><div><div class="container">
 		<jdoc:include type="modules" name="header" style="xhtml" />
-		
-	<img id="vaw_logo" src="../../images/default/vaw_logo.png" />
-	
-	<div id="navcontainer">
-		<ul class="menu">
-		<li><a href="#">Home</a></li>
-		<li><a href="#">About</a></li>
-		<li><a href="#">Member Agencies</a></li>
-		<li><a href="#">Events</a></li>
-		<li><a href="#">Contact</a></li>
-		</ul>
-	</div>
-		
-		
-		
+			
 	</div></div></div>
-		
+	
+	<?php if ($this->countModules('masthead')): ?>
 	<div id="masthead"><div class="container">
-				
-		<img class="floatleft" id="masthead_img" src="images/default/img_main.jpg">		
-		
-		<div id="masthead_ul">
-			<h1>A Community Working Together To End Violence Against Women</h1>
-			<ul class="big">
-				<li>Morbi vel lacus sed magna gravida mattis 
-					eu in nisl.</li>
-				<li>Sed ullamcorper scelerisque ornare.    
-				   Pellentesque convallis aliquam quam eget 
-				   pretium.</li>
-				<li>Praesent eget nisl libero.</li>
-			</ul>
-		</div>
+		<jdoc:include type="modules" name="masthead" style="xhtml" />
 		
 		<div class="clear"></div>
 	</div></div>
+	<?php endif; ?>
 		
 	<div id="body"><div class="container">
-	<div id="component">
-			
-			<img class="floatright" id="comp_img" src="images/default/img_comp.jpg" >
-			
-		<h1>Welcome to SLCCVAW!</h1>
-
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim neque a velit condimentum molestie.<br/><br/>
-
-		Fusce quis lacus arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim neque a velit condimentum molestie.<br/><br/>
-
-		Suspendisse leo nisl, tempus id mollis elementum, faucibus nec nulla. Nulla consectetur convallis erat, vulputate tincidunt nisl sodales sit amet. Aliquam tincidunt fermentum tincidunt. Etiam dignissim neque a velit condimentum molestie. Fusce quis lacus arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-		
-		
-	</div>
-	<div id="sidebar">
-		
-		<div>
-			<h1>Upcoming Events</h1>
-			<ul class="events">
-				<li>May 6, 2011<br />
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				</li>
-				
-				<li>May 23, 2011<br />
-				Etiam dignissim neque a velit condimentum molestie.</li>
-				
-				<li>June 1, 2011<br />
-				Fusce quis lacus arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-				
-			</ul>
+		<div id="component">
+			<jdoc:include type="component" />
 		</div>
+		<div id="sidebar">
+			<jdoc:include type="modules" name="sidebar" style="xhtml" />
 		
-	</div>
-	<div class="clear"></div>		
+		</div>
+		<div class="clear"></div>
 	</div></div>
 		
 	<div id="footer"><div class="container">
-		<div id="navfooter">
-			<ul class="menu">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Member Agencies</a></li>
-				<li><a href="#">Events</a></li>
-				<li><a href="#">Contact</a></li>
-			</ul>
-		</div>
-		<div class="floatleft">© 2011 SLCCVAW. All Rights Reserved.</div>
-		<div class="floatright">Site by CCI Studios</div>
+		<jdoc:include type="modules" name="footer" style="xhtml" />
+		
+		<div class="floatleft">&copy; <?php echo date('Y') ?> SLCCVAW. All Rights Reserved.</div>
+		<div class="floatright">Site by <a href="http://ccistudios.com" target="_blank">CCI Studios</a></div>
 		
 		<div class="clear"></div>
 	</div></div>
