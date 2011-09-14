eff23472babac307d9bc7ba9bd01e5a2aed48694
Matthew Elliott <melliott@ccistudios.com>
2011-09-14 11:03:10 -0400

    Organized module

--- public_html/modules/mod_slideshow/mod_slideshow.php
+++ public_html/modules/mod_slideshow/mod_slideshow.php
@@ -5,4 +5,26 @@ $doc =& JFactory::getDocument();
 $doc->addStyleSheet( 'media/mod_slideshow/slideshow.css' );
 $doc->addScript("media/mod_slideshow/slideshow.js");
 
+$path = '/images/default/gallery/';
+$files = JFolder::files(JPATH_SITE .$path);
+$images = '';
+foreach($files as $index=>$file) {
+	$images .= "'". $path.$file ."'";
+	if($index != count($files)-1) {
+		$images .= ',';
+	}
+}
+
+$doc->addScriptDeclaration("
+var ss;
+window.addEvent('domready', function() {
+	ss = new SimpleImageSlideShow({
+		imgUrls: [$images],
+		container: $('slideshow')
+	});
+	
+	ss.start();
+});
+");
+
 require JModuleHelper::getLayoutPath('mod_slideshow', $params->get('layout', 'default'));
--- public_html/modules/mod_slideshow/tmpl/default.php
+++ public_html/modules/mod_slideshow/tmpl/default.php
@@ -1,26 +1,3 @@
-<script>
-var ss;
-window.addEvent('domready', function() {
-	ss = new SimpleImageSlideShow({
-		imgUrls: [<?php
-
-		$path = '/images/default/gallery/';
-		$files = JFolder::files(JPATH_SITE .$path);
-		foreach($files as $index=>$file) {
-			echo "'".$path.$file."'";
-			if($index != count($files)-1) {
-				echo ',';
-			}
-		}
-		
-		?>],
-		container: $('slideshow')
-	});
-	
-	ss.start();
-});
-</script>
-
 <div id="slideshow">
 	<ul class="dots"></ul>
 </div>
